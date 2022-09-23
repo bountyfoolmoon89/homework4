d4930f71498c93f94284f4fd4136b9b3adc204cd
Егор <bountyfoolmoon89@yandex.com>
2022-09-23 23:26:30 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+       //Задача 1
+        System.out.println("Задача 1");
+        int a = 232;
+        byte b = 127;
+        short c = -111;
+        long d = 1234567890;
+        float e = 1.333f;
+        double f = 1.5;
+        System.out.println("Значение переменной a с типом int равно " + a);
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        System.out.println("Значение переменной c с типом short равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
+
+
+
+    }
+}
