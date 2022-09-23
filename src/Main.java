33d3598bfac72c0945125bd0c4d6d2b6b01240ef
Егор <bountyfoolmoon89@yandex.com>
2022-09-24 00:14:03 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       //Задача 1
+        //Задача 1
         System.out.println("Задача 1");
         int a = 232;
         byte b = 127;
@@ -14,6 +14,24 @@ public class Main {
         System.out.println("Значение переменной d с типом long равно " + d);
         System.out.println("Значение переменной e с типом float равно " + e);
         System.out.println("Значение переменной f с типом double равно " + f);
+        //Задача 2
+        System.out.println("Задача 2");
+        double first = 27.12;
+        long second = 98712312365549L;
+        float third = 2.7886f;
+        boolean fourth = false;
+        short fifth = 569;
+        int sixth = 27897;
+        int seventh = -159;
+        byte eighth = 67;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(fourth);
+        System.out.println(fifth);
+        System.out.println(sixth);
+        System.out.println(seventh);
+        System.out.println(eighth);
 
 
 
